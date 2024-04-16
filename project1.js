d07af93f90560095e3f53e3af39e58c0e2e5ad01
Ben <benblyth590@googlemail.com>
2024-04-16 12:33:19 +0100

    lum

--- project1.js
+++ project1.js
@@ -1,8 +1,11 @@
 document.querySelectorAll('.nav-link-modal').forEach(button => {
-    button.addEventListener('click', toggleModal)
-})
+    button.addEventListener('click', toggleModal);
+});
+
+// const closeIcon = document.querySelector('.fa-window-close.nav-link-modal');
+// closeIcon.addEventListener('click', window.close);
 
 function toggleModal(e) {
     e.preventDefault();
-    document.querySelector('.modal').classList.toggle('open')
+    document.querySelector('.modal').classList.toggle('open');
 }
