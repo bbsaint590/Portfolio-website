9944c0753ebc4a84d5ea5b3619a6b61249bdcc78
Ben <benblyth590@googlemail.com>
2024-04-16 14:17:20 +0100

    up to adding json

--- project1.js
+++ project1.js
@@ -1,9 +1,13 @@
-document.querySelectorAll('.nav-link-modal').forEach(button => {
+document.querySelectorAll('.nav-link-modal-skills').forEach(button => {
     button.addEventListener('click', toggleModal);
 });
 
 // const closeIcon = document.querySelector('.fa-window-close.nav-link-modal');
 // closeIcon.addEventListener('click', window.close);
+
+document.querySelectorAll('.nav-link-modal-exp').forEach(button => {
+    button.addEventListener('click', toggleModal);
+});
 
 function toggleModal(e) {
     e.preventDefault();
