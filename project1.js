6d0ffd0e64bed6eb3050563d5f6ad31f31bde921
Ben <benblyth590@googlemail.com>
2024-04-16 16:58:39 +0100

    end of day

--- project1.js
+++ project1.js
@@ -25,15 +25,128 @@ fetch('projects.json')
         return response.json()
     })
     .then((data) => {
-        data.data.forEach((project) => {
-            document.querySelector('#project1').innerHTML += `
+        data.map((project) => {
+            let projectCont = document.createElement("div")
+            projectCont.className = "project2";
+            projectCont.innerHTML = `
                 <div>
                     <h1>${project.Title}</h1>
                     <p>${project.Description}</p>
-                    <p>${project.image}</p>
-                    <p>${project.url}</p>
+                    <img src="${project.image}">
+                    <a href="${project.url}">Project Link</a>
                     <button class="project1-button" data-id="${project.id}">Select Project</button>
                 </div>
-            `;
-        });
-    });
+            `
+            // Assuming project.id is the id of the container where you want to append the project
+            let containerId = project.id < 0 ? project.id : "project"
+            document.getElementById(containerId).appendChild(projectCont)
+        })
+    })
+    
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch('projects.json')
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         data.forEach((project) => {
+//             document.querySelector('#project1').innerHTML += `
+//                 <div>
+//                     <h1>${project.Title}</h1>
+//                     <p>${project.Description}</p>
+//                     <img src="${project.image}">
+//                     <a href="${project.url}"></a>
+//                     <button class="project1-button" data-id="${project.id}">Select Project</button>
+//                 </div>
+//             `
+//         })
+//     })
+   
+
+
+    //         document.querySelector('#project2').innerHTML += `
+    //         <div>
+    //             <h1>${project.Title}</h1>
+    //             <p>${project.Description}</p>
+    //             <p>${project.image}</p>
+    //             <p>${project.url}</p>
+    //             <button class="project1-button" data-id="${project.id}">Select Project</button>
+    //         </div>
+    //     `
+    //     document.querySelector('#project3').innerHTML += `
+    //     <div>
+    //         <h1>${project.Title}</h1>
+    //         <p>${project.Description}</p>
+    //         <p>${project.image}</p>
+    //         <p>${project.url}</p>
+    //         <button class="project1-button" data-id="${project.id}">Select Project</button>
+    //     </div>
+    // `
+    //     })
+    // })
+
+//     fetch('projects.json')
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         const project = data.data[0]
+//         document.querySelector('#project1').innerHTML += `
+//         <div>
+//         <h1>${project.Title}</h1>
+//         <p>${project.Description}</p>
+//         <p>${project.image}</p>
+//         <p>${project.url}</p>
+//         <button class="project1-button" data-id="${project.id}">Select Project</button>
+//     </div>
+// `
+// })
+
+// fetch('projects.json')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     const project = data.data[1]
+//     document.querySelector('#project2').innerHTML += `
+//     <div>
+//     <h1>${project.Title}</h1>
+//     <p>${project.Description}</p>
+//     <p>${project.image}</p>
+//     <p>${project.url}</p>
+//     <button class="project1-button" data-id="${project.id}">Select Project</button>
+// </div>
+// `
+// })
+
+// fetch('projects.json')
+// .then((response) => {
+//     return response.json()
+// })
+// .then((data) => {
+//     const project = data.data[2]
+//     document.querySelector('#project3').innerHTML += `
+//     <div>
+//     <h1>${project.Title}</h1>
+//     <p>${project.Description}</p>
+//     <p>${project.image}</p>
+//     <p>${project.url}</p>
+//     <button class="project1-button" data-id="${project.id}">Select Project</button>
+// </div>
+// `
+// })
