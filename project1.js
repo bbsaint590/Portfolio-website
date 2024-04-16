c7ff549c19d95793ab4f70de388d4f08d6bc815c
Ben <benblyth590@googlemail.com>
2024-04-16 14:54:47 +0100

    json added

--- project1.js
+++ project1.js
@@ -13,3 +13,27 @@ function toggleModal(e) {
     e.preventDefault();
     document.querySelector('.modal').classList.toggle('open');
 }
+
+document.querySelectorAll('.project1-button').forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.dataset.id)
+    })
+})
+
+fetch('projects.json')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        data.data.forEach((project) => {
+            document.querySelector('#project1').innerHTML += `
+                <div>
+                    <h1>${project.Title}</h1>
+                    <p>${project.Description}</p>
+                    <p>${project.image}</p>
+                    <p>${project.url}</p>
+                    <button class="project1-button" data-id="${project.id}">Select Project</button>
+                </div>
+            `;
+        });
+    });
