8201d6e220b6ac4941ba281fb5e3f10491e396ca
Ben <benblyth590@googlemail.com>
2024-04-17 12:31:40 +0100

    before lunch

--- project1.js
+++ project1.js
@@ -1,20 +1,17 @@
 document.querySelectorAll('.nav-link-modal-skills').forEach(button => {
-    button.addEventListener('click', toggleModal);
-});
+    button.addEventListener('click', () => toggleModal('.modalSkill'))
+})
 
 document.querySelectorAll('.nav-link-modal-exp').forEach(button => {
-    button.addEventListener('click', toggleModal);
-});
+    button.addEventListener('click', () => toggleModal('.modalExp'))
+})
 
-function toggleModal(e) {
-    e.preventDefault();
-    document.querySelector('.modalSkill').classList.toggle('open');
+function toggleModal(modalClass) {
+    document.querySelectorAll(modalClass).forEach(modal => {
+        modal.classList.toggle('open')
+    })
 }
 
-function toggleModal(e) {
-    e.preventDefault();
-    document.querySelector('.modalExp').classList.toggle('open');
-}
 
 fetch('exp.json')
     .then((response) => {
@@ -23,49 +20,52 @@ fetch('exp.json')
     .then((data) => {
         data.data.forEach((exp) => {
             document.querySelector(".modalExp").innerHTML += `
-                            <div class='experience'>
-                                 <h1>${exp.Role}</h1>
-                               <p>${exp.Where}</p>
-                               <p>${exp.When}</p>
-                               <p>${exp.Responsibilities}</p>
-                                 <button class="project1-button" data-id="${exp.id}">Select Project</button>
-                            </div>`
-        })
-    })
+                <div class='experience'>
+                    <h1>${exp.Role}</h1>
+                    <p>${exp.Where}</p>
+                    <p>${exp.When}</p>
+                    <p>${exp.Responsibilities}</p>
+                </div>`;
+        });
+    });
 
-    fetch('skills.json')
+fetch('skills.json')
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         data.data.forEach((skills) => {
             document.querySelector(".modalSkill").innerHTML += `
-                            <div class='skills'>
-                                 <h1>${skills.Skill}</h1>
-                               <p>${skills.Achieved}</p>
-                               <p>${skills.Where}</p>
-                               <p>${skills.When}</p>
-                                 <button class="project1-button" data-id="${skills.id}">Select Project</button>
-                            </div>`
-        })
-    })
+                <div class='skills'>
+                    <h1>${skills.Skill}</h1>
+                    <p>${skills.Achieved}</p>
+                    <p>${skills.Where}</p>
+                    <p>${skills.When}</p>
+                </div>`;
+        });
+    });
 
-    fetch('projects.json')
+fetch('projects.json')
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         data.data.forEach((project) => {
             document.querySelector(".projects").innerHTML += `
-                            <div class='projectBox'>
-                                 <h1>${project.Title}</h1>
-                               <p>${project.Description}</p>
-                                <img src="${project.image}">
-                             <a href="${project.url}"></a>
-                                 <button class="project1-button" data-id="${project.id}">Select Project</button>
-                            </div>`
-        })
-    })
+            <div class='projectBox'>
+                <div class="title.pic>
+                    <h1>${project.Title}</h1>
+                    <img class = "pic" src="${project.image}">
+                </div>
+                <div class="right">
+                    <p>${project.Description}</p>
+                    <a href="${project.url}">Live</a>
+                    <a href="${project.github}"><i class="fa-brands fa-github"></i>Github</a>
+                </div>
+            </div>`;
+        });
+    });
+
 
 
 
