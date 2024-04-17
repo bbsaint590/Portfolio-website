7c313304df89e991c549f843a560d5d16711526f
Ben <benblyth590@googlemail.com>
2024-04-17 17:04:53 +0100

    end of wed

--- project1.js
+++ project1.js
@@ -6,12 +6,15 @@ document.querySelectorAll('.nav-link-modal-exp').forEach(button => {
     button.addEventListener('click', () => toggleModal('.modalExp'))
 })
 
+document.querySelectorAll('.project-button').forEach(button => {
+    button.addEventListener('click', () => toggleModal('.modalProject'))
+})
+
 function toggleModal(modalClass) {
     document.querySelectorAll(modalClass).forEach(modal => {
         modal.classList.toggle('open')
     })
 }
-
 
 fetch('exp.json')
     .then((response) => {
@@ -53,28 +56,63 @@ fetch('projects.json')
         data.data.forEach((project) => {
             document.querySelector(".projects").innerHTML += `
             <div class='projectBox'>
-                <div class="title.pic>
-                    <h1>${project.Title}</h1>
+                <div class="title-pic">
+                    <h2>${project.Title}</h2>
                     <img class = "pic" src="${project.image}">
+                </div>     
+                <div class="project-links">  
+                    <button class="project-button">Select Project</button>
                 </div>
-                <div class="right">
-                    <p>${project.Description}</p>
-                    <a href="${project.url}">Live</a>
-                    <a href="${project.github}"><i class="fa-brands fa-github"></i>Github</a>
-                </div>
-            </div>`;
+            `;
+        });
+        data.data.forEach((project) => {
+            document.querySelector(".modalProject").innerHTML += `
+            <div class="modalProject">
+            <p>${project.Description}</p>
+            <a href="${project.url}">Live</a>
+            <a href="${project.github}"><i class="fa-brands fa-github"></i>Github</a>
+            </div>
+            `;
         });
     });
 
+        data.data.forEach((project) => {
+            document.querySelector(".modalProject").innerHTML += `
+            <div class="modalProject">
+            <p>${project.Description}</p>
+            <a href="${project.url}">Live</a>
+            <a href="${project.github}"><i class="fa-brands fa-github"></i>Github</a>
+            </div>
+            `;
+        });
 
 
 
-// document.querySelectorAll('.project1-button').forEach((button) => {
-//     button.addEventListener('click', () => {
-//         console.log(button.dataset.id)
+
+
+// fetch('projects.json')
+//     .then((response) => {
+//         return response.json();
 //     })
-// })
-
+//     .then((data) => {
+//         data.data.forEach((project) => {
+//             document.querySelector(".projects").innerHTML += `
+//             <div class='projectBox'>
+//                 <div class="title-pic">
+//                     <h2>${project.Title}</h2>
+//                     <img class = "pic" src="${project.image}">
+//                 </div>
+//                 <div class="right">
+//                     <p>${project.Description}</p>
+//                     <div class="project-links">
+//                     <a href="${project.url}">Live</a>
+//                     <a href="${project.github}"><i class="fa-brands fa-github"></i>Github</a>
+//                     <button class="project-button" data-id="${project.Description.url.github}">Select Project</button>
+//                     </div>
+//                 </div>
+//             </div>`;
+//         });
+//     });
 
 
 
